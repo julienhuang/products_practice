f33809848b97a617733684738e85b9e31afe60ee
Jilien Huang <chiayen.huang@gmail.com>
2019-01-27 21:07:05 +0800

    first commit

---
+++ products_practice.py
@@ -0,0 +1,11 @@
+#記帳程式
+products = []
+while True:
+	name = input('請輸入商品名稱:')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格:')
+	products.append([name,price])
+print(products)
+
+print(products[0][1])
