2e6ec6bcedd91aeeda6db866b9189ad9b6cbe73e
Jilien Huang <chiayen.huang@gmail.com>
2019-01-28 15:32:37 +0800

    added path.isfile search

--- products_practice.py
+++ products_practice.py
@@ -1,24 +1,33 @@
-# 讀取檔案
-products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品, 價格' in line:
-			continue #繼續. 跳過此一回迴圈, 進入下一回
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+import os # 首先匯入"作業系統"模組(為了取用接下來的step1尋找檔案"isfile"功能)
 
-# 讓使用者輸入
+products = []
+
+# step 1: (在特定資料夾底下)尋找檔案
+if os.path.isfile('products.csv'): # os模組底下的 path模組底下的 isfile功能
+	print('找到檔案了!')
+
+# step 2: 讀取檔案
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #繼續. 跳過此一回迴圈, 進入下一回
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案...')
+
+# step 3: 讓使用者輸入購買紀錄
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')
-	products.append([name, price])
+	products.append([name, price]) # 以中括號方式將不同變數存入同一筆清單中, 等於二維清單two-dimensional list
+
 print(products)
 
-print(products[0][1]) 
-# 以中括號方式將不同變數存入同一筆清單中, 等於二維清單two-dimensional list
+print(products[0][1]) # 印出清單第一筆(包含兩項文字資料)的第二項資料
 
 # 印出所有購買紀錄
 for product in products:
